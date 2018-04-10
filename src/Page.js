a755b51a749eb490ecd6fb3a97969a6e5958400c
Kyle VanderBeek <kylev@kylev.com>
2018-04-10 11:45:38 -0700

    Remove boilerplate and create placeholder shims.

---
+++ src/Page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = () => {
+  return <svg>
+    <circle cx={40} cy={40} r={20} fill="red"/>
+    </svg>;
+}
+
+export default Page;
---
+++ src/SettingsForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SettingsForm = () => {
+  return (<div><form><input defaultValue={40}/></form></div>);
+};
+
+export default SettingsForm;
