c6e972d135fa7aeb894d27f92bd91c2d337bdb1b
Kyle VanderBeek <kylev@kylev.com>
2018-04-11 19:27:12 -0700

    Make pretty form.

--- src/PaperSettings.js
+++ src/PaperSettings.js
@@ -1,31 +1,37 @@
 import React from "react";
 import { observer } from "mobx-react";
+import { Form } from "antd";
 
 import DropDown from "./DropDown";
 import NumberInput from "./NumberInput";
 
 const PaperSettings = ({ store }) => {
+  const itemStyle = { labelCol: { span: 8 }, wrapperCol: { span: 16 } };
   return (
-    <form>
-      <DropDown
-        onChange={v => (store.pageSize = v)}
-        value={store.pageSize}
-        choices={store.pageSizes}
-      />
-      <br />
-      <DropDown
-        onChange={v => (store.orientation = v)}
-        value={store.orientation}
-        choices={store.orientations}
-      />
-      <br />
-      <NumberInput
-        min={0}
-        max={20}
-        value={store.margin}
-        onChange={v => (store.margin = v)}
-      />
-    </form>
+    <Form>
+      <Form.Item label="Size" {...itemStyle}>
+        <DropDown
+          onChange={v => (store.pageSize = v)}
+          value={store.pageSize}
+          choices={store.pageSizes}
+        />
+      </Form.Item>
+      <Form.Item label="Orentation" {...itemStyle}>
+        <DropDown
+          onChange={v => (store.orientation = v)}
+          value={store.orientation}
+          choices={store.orientations}
+        />
+      </Form.Item>
+      <Form.Item label="Margin" {...itemStyle}>
+        <NumberInput
+          min={0}
+          max={20}
+          value={store.margin}
+          onChange={v => (store.margin = v)}
+        />
+      </Form.Item>
+    </Form>
   );
 };
 
