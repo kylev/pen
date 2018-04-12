66450ea67662f92926943830f1ec42e654ee42a0
Kyle VanderBeek <kylev@kylev.com>
2018-04-12 13:47:00 -0700

    Show more debug data.

--- src/LineSetDebug.js
+++ src/LineSetDebug.js
@@ -2,13 +2,23 @@ import React from "react";
 import { observer } from "mobx-react";
 import { Row, Col } from "antd";
 
+const showFields = [
+  "x1",
+  "y1",
+  "x2",
+  "y2",
+  "stroke",
+  "strokeDasharray",
+  "strokeWidth"
+];
+
 const LineItem = ({ line }) => {
   return (
     <div>
       <Row key="name">
         <Col span={24}>Line: {line.key}</Col>
       </Row>
-      {["x1", "y1", "x2", "y2", "stroke", "strokeDasharray"].map(key => (
+      {showFields.map(key => (
         <Row key={key}>
           <Col span={14}>{key}</Col>
           <Col span={10}>{line[key]}</Col>
