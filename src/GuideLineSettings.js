4a0a7bab1b892aa5980b8641900afbbd5b986a1a
Kyle VanderBeek <kylev@kylev.com>
2018-04-12 17:45:21 -0700

    Unused import

--- src/GuideLineSettings.js
+++ src/GuideLineSettings.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { Col, Form, Row } from "antd";
+import { Col, Row } from "antd";
 
 import ColFormItem from "./ColFormItem";
 import DropDown from "./DropDown";
