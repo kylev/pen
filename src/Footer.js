c64b0473904c21a04ab1d12a6e542351964b59d9
Kyle VanderBeek <kylev@kylev.com>
2018-04-16 15:59:55 -0700

    Add fontawesome as a general icon solution, fixing footer links.

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,22 @@
 import React from "react";
 
 import { Typography } from "material-ui";
+import { withStyles } from "material-ui/styles";
 import Grid from "material-ui/Grid";
-import Icon from "material-ui/Icon";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import githubIcon from "@fortawesome/fontawesome-free-brands/faGithub";
+import twitterIcon from "@fortawesome/fontawesome-free-brands/faTwitter";
 
-const Footer = () => {
+const Footer = ({ classes }) => {
   return (
     <Grid container justify="center" spacing={24}>
       <Grid item xs={8}>
         <Typography align="center">
           <a href="https://github.com/kylev/pen">
-            <Icon>github</Icon>
+            <FontAwesomeIcon icon={githubIcon} className={classes.icon} />
           </a>
           <a href="https://twitter.com/kylev">
-            <Icon>twitter</Icon>
+            <FontAwesomeIcon icon={twitterIcon} className={classes.icon} />
           </a>
           Kyle V ©2018 Created with <a href="https://reactjs.org/">React</a>
           {" and "}
@@ -26,4 +29,10 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+const styles = theme => {
+  return {
+    icon: { paddingRight: theme.spacing.unit, paddingLeft: theme.spacing.unit }
+  };
+};
+
+export default withStyles(styles)(Footer);
