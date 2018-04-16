552020c00dd9103cc4f01e1e1caf600f4a05d876
Kyle VanderBeek <kylev@kylev.com>
2018-04-16 14:15:52 -0700

    Fix up Footer.

--- src/Footer.js
+++ src/Footer.js
@@ -1,24 +1,28 @@
 import React from "react";
 
 import { Typography } from "material-ui";
+import Grid from "material-ui/Grid";
 import Icon from "material-ui/Icon";
 
 const Footer = () => {
   return (
-    <Typography>
-      <a href="https://github.com/kylev/pen">
-        <Icon>github</Icon>
-      </a>
-      <a href="https://twitter.com/kylev">
-        <Icon>twitter</Icon>
-      </a>
-      Kyle V ©2018 Created with <a href="https://ant.design/">Ant Design</a>,{" "}
-      <a href="https://reactjs.org/">React</a>
-      {" and "}
-      <a href="https://mobx.js.org">Mobx</a> while thinking about my mom, a
-      teacher.{" "}
-      <a href="https://github.com/kylev/pen/issues">Bugs? Suggestions?</a>
-    </Typography>
+    <Grid container justify="center">
+      <Grid item xs={8}>
+        <Typography align="center">
+          <a href="https://github.com/kylev/pen">
+            <Icon>github</Icon>
+          </a>
+          <a href="https://twitter.com/kylev">
+            <Icon>twitter</Icon>
+          </a>
+          Kyle V ©2018 Created with <a href="https://reactjs.org/">React</a>
+          {" and "}
+          <a href="https://mobx.js.org">Mobx</a> while thinking about my mom, a
+          teacher.<br />
+          <a href="https://github.com/kylev/pen/issues">Bugs? Suggestions?</a>
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
