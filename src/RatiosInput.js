f0f524d87dee1ceef7e6e66389d91c5207407e46
Kyle VanderBeek <kylev@kylev.com>
2018-04-17 13:13:30 -0700

    Compose a custom ratio control.

--- src/RatiosInput.js
+++ src/RatiosInput.js
@@ -2,24 +2,39 @@ import React from "react";
 import { clamp } from "lodash";
 import { observer } from "mobx-react";
 
-import TextField from "material-ui/TextField";
+import { FormControl, FormHelperText } from "material-ui/Form";
+import Input, { InputLabel } from "material-ui/Input";
 
 const labels = ["Ascender", "X", "Descender", "Gap"];
 
+let RatioInput = ({ store }) => (
+  <span>
+    {store.ratios.map((r, i) => (
+      <span key={i} style={{ display: "inline-block", marginRight: 8 }}>
+        <Input
+          type="number"
+          value={r}
+          onChange={e => {
+            store.ratioPreset("custom");
+            store.ratios[i] = clamp(e.target.value, 0, 10);
+          }}
+          style={{ marginTop: 16, width: 60 }}
+        />
+        <br />
+        <FormHelperText>{labels[i]}</FormHelperText>
+      </span>
+    ))}
+  </span>
+);
+RatioInput = observer(RatioInput);
+
 const RatiosInput = ({ store }) => {
-  return store.ratios.map((r, i) => (
-    <TextField
-      type="number"
-      label={labels[i]}
-      value={r}
-      onChange={e => {
-        store.ratioPreset("custom");
-        store.ratios[i] = clamp(e.target.value, 0, 10);
-      }}
-      style={{ marginRight: 10, width: 100 }}
-      key={i}
-    />
-  ));
+  return (
+    <FormControl>
+      <InputLabel shrink>Ratios</InputLabel>
+      <RatioInput store={store} />
+    </FormControl>
+  );
 };
 
 export default observer(RatiosInput);
