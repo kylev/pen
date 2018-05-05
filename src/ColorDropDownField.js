be42aaf86fc94f453a0a7fe8f1e83640dfef1670
Kyle VanderBeek <kylev@kylev.com>
2018-05-04 17:15:36 -0700

    Non-HTML colors specify keys differently.

--- src/ColorDropDownField.js
+++ src/ColorDropDownField.js
@@ -16,11 +16,11 @@ const colors = [
     name: `Gray ${v * 10}%`
   })),
   { key: "blue" },
+  { key: Color.rgb(164, 221, 237).string(), name: "colors.nonphotoblue" },
   { key: "cyan" },
   { key: "darkgray" },
   { key: "green" },
   { key: "lightgray" },
-  { key: Color.rgb(164, 221, 237).string(), name: "Non-Photo Blue" },
   { key: "pink" },
   { key: "red" },
   { key: "white" },
