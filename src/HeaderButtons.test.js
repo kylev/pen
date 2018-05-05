3c2311476beaf5d4e44b43011ec282a84747de4f
Kyle VanderBeek <kylev@kylev.com>
2018-05-04 23:04:50 -0700

    We need all the i18n to make this test work for now.

--- src/HeaderButtons.test.js
+++ src/HeaderButtons.test.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { I18nextProvider } from "react-i18next";
 import { mount } from "enzyme";
 
+import i18n from "./i18n";
 import HeaderButtons from "./HeaderButtons";
 
 it("renders 3 buttons", () => {
-  const div = mount(<HeaderButtons />);
+  const div = mount(
+    <I18nextProvider i18n={i18n}>
+      <HeaderButtons />
+    </I18nextProvider>
+  );
+
   expect(div.find("button")).toHaveLength(3);
 });
