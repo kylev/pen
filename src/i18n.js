92c505a85d4a81e0086b2c511dea165852e4ead5
Kyle VanderBeek <kylev@kylev.com>
2018-05-04 23:05:13 -0700

    Use a special fallback in CI.

--- src/i18n.js
+++ src/i18n.js
@@ -4,8 +4,11 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import enDefault from "./locales/en/default";
 import esDefault from "./locales/es/default";
 
+let fallbackLng = "en";
+if (process.env.NODE_ENV === "test") fallbackLng = "cimode";
+
 i18n.use(LanguageDetector).init({
-  fallbackLng: "en",
+  fallbackLng,
   debug: process.env.NODE_ENV === "development",
   resources: {
     en: { translation: enDefault },
