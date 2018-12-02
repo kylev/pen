b25338e7e4a0a6263d1015449ced823ec1853a50
Travis CI User <travis@example.org>
2018-12-02 01:48:59 +0000

    kylev/pen@4b6e44640753c76e963c6e0d0a5c78465c90ac2c Travis 60

---
+++ precache-manifest.2159491b4a12171ec51c5cc3be4225ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fc59a307ff69367661d",
+    "url": "/pen/static/js/runtime~main.8fc59a30.js"
+  },
+  {
+    "revision": "841771d385bbf40c8902",
+    "url": "/pen/static/js/main.841771d3.chunk.js"
+  },
+  {
+    "revision": "6acf1a5381425ff97779",
+    "url": "/pen/static/js/1.6acf1a53.chunk.js"
+  },
+  {
+    "revision": "841771d385bbf40c8902",
+    "url": "/pen/static/css/main.6c7ca97e.chunk.css"
+  },
+  {
+    "revision": "f888260e61ff6eaf2ec4276950598cb2",
+    "url": "/pen/index.html"
+  }
+];
