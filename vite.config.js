135440c97736e97eebb8f7acf95aa48760f3fa06
Kyle VanderBeek <kylev@kylev.com>
2025-07-28 16:03:06 -0700

    Relative URLs for github.io pathed project

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { use } from 'react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { use } from "react";
 
 export default defineConfig({
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-    server: {
-      host: true,
-    },
+  base: "./",
+  build: {
+    outDir: "build",
+  },
+  plugins: [react()],
+  server: {
+    host: true,
+  },
 });
