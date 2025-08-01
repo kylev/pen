455f7479e3f68a6142c0bc4852d48bc7ccc1e6ac
Kyle VanderBeek <kylev@kylev.com>
2025-08-01 13:09:32 -0700

    Normal base config
    https://vite.dev/guide/static-deploy.html#github-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import observerPlugin from "mobx-react-observer/babel-plugin";
 
 export default defineConfig({
-  base: "./",
+  base: "/pen/",
   build: {
     rollupOptions: {
       output: {
